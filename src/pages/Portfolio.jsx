92bd71db350f539452ceebee0eb7841123ba1f0d
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-07-19 02:34:07 -0700

    `Update Portfolio.jsx: modify inline div class name and properties`

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,7 +6,7 @@ export default function Portfolio() {
   return (
     <div className="w-full mt-20 section">
       <div className="space-y-4 text-left">
-        <div className="inline- justify-center py-3 flex md:m-0 m-auto border rounded-full w-fit !-z-10 border-neutral-800 backdrop-blur-sm bg-neutral-900/30">
+        <div className="inline- justify-center md:mr-0 mr-5 py-3 flex md:m-0 m-auto border rounded-full w-fit !-z-10 border-neutral-800 backdrop-blur-sm bg-neutral-900/30">
           <FuzzyText baseIntensity={0.15} hoverIntensity={1} enableHover={true} fontSize="clamp(2rem, 10vw, 3.5rem)">
             Featured Projects
           </FuzzyText>
