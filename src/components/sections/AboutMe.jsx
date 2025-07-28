0923a2b2a4eef5de1008582d2db50261744ccb29
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-07-28 16:10:31 -0700

    `Added ButtonTwo component and download resume link to AboutMe section`

--- src/components/sections/AboutMe.jsx
+++ src/components/sections/AboutMe.jsx
@@ -2,14 +2,16 @@ import FuzzyText from "../animations/FuzzyText";
 import ProfileCard from "../animations/ProfileCard/ProfileCard";
 import myImage from "../../assets/images/avatar.png";
 import { useState } from "react";
+import { ButtonTwo } from "../ui/Button";
+import { BiDownload } from "react-icons/bi";
 
 export default function AboutMe() {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <div className="w-full flex items-center gap-20 sm:flex-nowrap flex-wrap">
+    <div className="flex flex-wrap items-center w-full gap-20 sm:flex-nowrap">
       <div className="space-y-4 text-left">
-        <div 
+        <div
           data-aos="fade-down"
           className="inline- justify-center md:mr-0 mr-5 py-3 flex md:m-0 m-auto border rounded-full w-fit !-z-10 border-neutral-800 backdrop-blur-sm bg-neutral-900/30"
         >
@@ -37,25 +39,20 @@ export default function AboutMe() {
           I bring innovative ideas to life. Let's connect and work together to turn your vision into a compelling digital reality!
         </article>
 
-        <div 
-          data-aos="fade-up"
-          data-aos-delay="400"
-          className="flex justify-center mt-4 lg:hidden"
-        >
+        <div data-aos="fade-up" data-aos-delay="400" className="flex justify-center mt-4 lg:hidden">
           <button
             onClick={() => setIsExpanded(!isExpanded)}
-            className="text-neutral-100 hover:text-neutral-300 text-4xl font-medium transition-colors"
+            className="text-4xl font-medium transition-colors text-neutral-100 hover:text-neutral-300"
           >
             {isExpanded ? "Read less..." : "Read more..."}
           </button>
         </div>
+        <div className="!mt-20 flex items-center justify-center w-full">
+          <ButtonTwo children="resume" icon={<BiDownload size={20} />} title="Download PDF resume" aria="Download resume" />
+        </div>
       </div>
 
-      <div 
-        data-aos="fade-left"
-        data-aos-delay="200"
-        className="m-auto w-fit flex justify-center md:mt-0 mt-16"
-      >
+      <div data-aos="fade-left" data-aos-delay="200" className="flex justify-center m-auto mt-16 w-fit md:mt-0">
         <ProfileCard
           name="Joshua Onyeka"
           title="Software Developer"
