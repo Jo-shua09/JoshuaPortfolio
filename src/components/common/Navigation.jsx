7e365af0474258e3f6646043f2c16e5795346d5c
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-07-28 12:27:59 -0700

    `Update Navigation component header styles: changed py-8 to py-12`

--- src/components/common/Navigation.jsx
+++ src/components/common/Navigation.jsx
@@ -38,7 +38,7 @@ export default function Navigation() {
   }, []);
 
   return (
-    <header className="w-full py-8 !h-fit">
+    <header className="w-full py-12 !h-fit">
       <div className="md:max-w-[100rem] max-w-6xl !h-fit w-[85%] fixed top-8 left-0 right-0 z-[9999] p-10 md:w-full rounded-2xl border border-neutral-500/30 m-auto backdrop-blur-3xl bg-neutral-900/95">
         <nav className="flex items-center justify-between">
           <div className="w-full h-full">
