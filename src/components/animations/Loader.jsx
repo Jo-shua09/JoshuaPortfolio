28831a67e3f54e0be5127d3ccc3f2a11c0c1b953
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-09-03 16:34:21 -0700

    fix: update Loader component styles for improved layout and visual consistency

--- src/components/animations/Loader.jsx
+++ src/components/animations/Loader.jsx
@@ -18,9 +18,9 @@ export default function Loader({ onLoadingComplete }) {
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center w-screen h-screen bg-neutral-950 wrapper">
-      <div className="container grid w-32 grid-cols-3 gap-1 rotate-45 shadow-2xl">
+      <div className="container grid w-32 grid-cols-3 gap-2 rotate-45 shadow-2xl">
         {new Array(9).fill().map((arr) => {
-          return <div className="w-10 h-10 shadow-2xl box bg-neutral-500"></div>;
+          return <div className="w-10 h-10 rounded shadow-2xl box bg-neutral-500"></div>;
         })}
       </div>
     </div>
